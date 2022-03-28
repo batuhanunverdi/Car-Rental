ca8a8553cd5dd82593ba5ad3ce81e7fcf7aaa15e
Mert Batuhan Unverdi <mertbatuhanu@hotmail.com>
2022-03-29 01:32:16 +0300

    Navbar and Search Form Added

---
+++ js/script.js
@@ -0,0 +1,15 @@
+        var nav = document.querySelector('nav');
+        window.addEventListener('scroll', function () {
+            if (window.pageYOffset > 100) {
+                nav.classList.add('bg-dark', 'shadow');
+            } else {
+                nav.classList.remove('bg-dark', 'shadow');
+            }
+        });
+
+        $(function() {
+            $('#startDatePicker').datepicker();
+        });
+        $(function() {
+            $('#endDatePicker').datepicker();
+        });
